2167d3d68333d0f7ef40e95c1e70327f571c419b
Usama Usman <uusman004@gmail.com>
2023-11-13 16:27:44 +0500

    FEAT:Basic Structure

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send("Hello I am " + req.body.name)
+
+})
+
+app.post('/login', (req, res) => {
+    res.send('Hello I am POST!')
+})
+
+app.put('/', (req, res) => {
+    res.send('Hello I am PUT!')
+})
+
+app.delete('/', (req, res) => {
+    res.send('Hello I am DELETE!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
